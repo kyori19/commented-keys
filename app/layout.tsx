cc2570f798ea76b8ae178b60420e312cd636fee8
kyori19 <kyori@accelf.net>
2022-11-13 21:49:49 +0000

    Add hero image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,21 @@
 import './global.css';
 import {ReactNode} from "react";
+import {JetBrains_Mono} from "@next/font/google";
+
+const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html>
-        <head></head>
+        <head>
+            <style>
+                {`
+                    html, pre {
+                        font-family: ${jetBrainsMono.style.fontFamily}, monospace;
+                    }
+                `}
+            </style>
+        </head>
         <body>{children}</body>
         </html>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
-const Page = () => null;
+import Head from "next/head";
+
+import styles from './styles.module.scss';
+import CommentedKeysIcon from './commented-keys.svg';
+
+const Page = () => (<>
+    <Head>
+        <title>commented-keys - Fetch commented authorized_keys</title>
+        <meta name="description" content="Fetch authorized_keys with comments from providers such as GitHub" />
+    </Head>
+
+    <main>
+        <pre className={styles.hero}>
+            <span className={styles.comment}># Fetch your SSH public keys instantly on your server!</span>
+            <div>
+                <span className={styles.title}>
+                    <CommentedKeysIcon viewBox="0 0 640 640" />
+                    commented-keys
+                </span>
+                <p className={styles.ghost}>
+                    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGGLuoOhA2CegyoITnZbzHkX9B<br />
+                    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYRVadSYWlBjXq2x6kICXSMemjX<br />
+                    ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAy<br />
+                    ssh-dss AAAAB3NzaC1kc3MAAACBALi6+Sdl4XcvQ6K8p3I7HA+gxmGxVaBj3AZp
+                </p>
+            </div>
+            <span className={styles.comment}># Comment your authorized_keys fetched from GitHub!</span>
+        </pre>
+    </main>
+</>);
 
 export default Page;
